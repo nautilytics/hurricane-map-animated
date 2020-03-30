0f368ccb555525dae53dfc275befbcf538c1030f
Chris Lanoue <chris@nautilytics.com>
2020-03-30 09:29:17 -0400

    fixed issue w/ last index not displaying correctly

--- src/components/App/Maps/Map/WindSpeed/index.js
+++ src/components/App/Maps/Map/WindSpeed/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { dateAccessor, getColor, xAccessor } from '../../../../../utils';
+import { dateAccessor, getColor, idAccessor, xAccessor } from '../../../../../utils';
 import moment from 'moment';
 import { DATE_TIME_FORMAT } from '../../../../../constant';
+import { ascending } from 'd3-array';
 
 const WindSpeed = ({ path }) => {
   const colorScale = useSelector(state => state.global.colorScale);
@@ -14,7 +15,8 @@ const WindSpeed = ({ path }) => {
   return (
     <g className="wind-speed">
       {windSpeed
-        .filter(d => moment(dateAccessor(d), DATE_TIME_FORMAT).isBefore(dt))
+        .filter(d => moment(dateAccessor(d), DATE_TIME_FORMAT).isSameOrBefore(dt))
+        .sort((a, b) => ascending(idAccessor(a), idAccessor(b)))
         .map(feature => {
           return (
             <path
