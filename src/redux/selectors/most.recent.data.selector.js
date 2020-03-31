f7aa6864d8b6d6a60ad618e1707b045300acc932
Chris Lanoue <chris@nautilytics.com>
2020-03-30 20:30:51 -0400

    show the cumulative counties over time

--- src/redux/selectors/most.recent.data.selector.js
+++ src/redux/selectors/most.recent.data.selector.js
@@ -1,14 +1,22 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
+import moment from 'moment';
+import { DATE_TIME_FORMAT } from '../../constant';
 
 const nestedDataSelector = state => state.global.nestedData;
 const currentIndexSelector = state => state.global.currentIndex;
 const timePeriodsSelector = state => state.global.timePeriods;
 
 const mostRecentDataSelector = createSelector(
-    [nestedDataSelector, currentIndexSelector, timePeriodsSelector],
-    (data, currentIndex, timePeriods) => {
-        const xs = data.find(d => d.key === timePeriods[currentIndex]);
-        return xs ? xs.values : [];
-    });
+  [nestedDataSelector, currentIndexSelector, timePeriodsSelector],
+  (data, currentIndex, timePeriods) => {
+    const xs = data.filter(d => moment(d.key, DATE_TIME_FORMAT).isSameOrBefore(moment(timePeriods[currentIndex], DATE_TIME_FORMAT)));
+    return xs
+      ? [].concat.apply(
+          [],
+          xs.map(x => x.values),
+        )
+      : [];
+  },
+);
 
 export default mostRecentDataSelector;
