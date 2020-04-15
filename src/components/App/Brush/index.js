a0217f7ab7bba2da5dc40db50b4ada5bfb10e83c
Chris Lanoue <chris@nautilytics.com>
2020-04-15 11:17:01 -0400

    add local value for smoother handling of slider dragging

--- src/components/App/Brush/index.js
+++ src/components/App/Brush/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Slider } from '@material-ui/core';
 import moment from 'moment';
@@ -12,11 +12,16 @@ const Brush = () => {
   const dispatch = useDispatch();
   const timePeriods = useSelector(state => state.global.timePeriods);
   const currentIndex = useSelector(state => state.global.currentIndex);
+  const [localValue, setLocalValue] = useState(currentIndex);
   const tickTimer = useCallback(() => dispatch({ type: TIMER_TICK }), [dispatch]);
   const minValue = moment(timePeriods[0], DATE_TIME_FORMAT);
   const maxValue = moment(timePeriods[timePeriods.length - 1], DATE_TIME_FORMAT);
   const [timer, setTimer] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    setLocalValue(currentIndex);
+  }, [currentIndex]);
 
   const marks = timePeriods.map(timePeriod => {
     return {
@@ -35,6 +40,10 @@ const Brush = () => {
     },
     [dispatch],
   );
+
+  const updateOnDrag = (evt, newValue) => {
+    setLocalValue(marks.findIndex(d => d.value === newValue));
+  };
 
   const start = () => {
     setTimer(
@@ -55,8 +64,9 @@ const Brush = () => {
       {isPlaying ? <PauseCircleOutline fontSize={'large'} onClick={stop} /> : <PlayCircleOutline fontSize={'large'} onClick={start} />}
       <div className="label">{minValue.format('MMM DD')}</div>
       <Slider
-        value={moment(timePeriods[currentIndex], DATE_TIME_FORMAT).valueOf()}
+        value={moment(timePeriods[localValue], DATE_TIME_FORMAT).valueOf()}
         onChangeCommitted={updateCurrentIndexOnDragEnd}
+        onChange={updateOnDrag}
         valueLabelDisplay="on"
         valueLabelFormat={x => moment(x).format(DISPLAY_DATE_FORMAT)}
         aria-labelledby="discrete-slider-restrict"
